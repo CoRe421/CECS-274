65741438fa94f0af313ef7307a8d8e44f5f53d6c
CoRe421 <48600499+CoRe421@users.noreply.github.com>
2019-09-13 12:45:58 -0700

    Project 1

--- RecipeBook.java
+++ RecipeBook.java
@@ -1,13 +1,12 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
-import java.lang.Math;
 /**
  * A class that contains an array of Recipe objects, including methods that can be called upon from another program.
  */
 public class RecipeBook {
     private Recipe[] mRecipeBook;
-    private int mCount;
+
     /**
      * Takes a file given by the user and reads every line of the file, creating Recipe objects and putting them into an array.
      * @param fileName The name of the file containing the recipes, must be an existing file that is formatted correctly.
@@ -16,17 +15,13 @@ public class RecipeBook {
         File textFile = new File(fileName);
         try {
             Scanner input = new Scanner(textFile);
-            mCount = input.nextInt();
-            int length = (int)Math.round(Math.pow(2,Math.ceil(Math.log((double)mCount)/Math.log(2))));
-            mRecipeBook = new Recipe[length];
+            mRecipeBook = new Recipe[input.nextInt()];
             input.nextLine();
-            for (int i = 0; i < mCount; i++) {
+            for (int i = 0; i < mRecipeBook.length; i++) {
                 String line = input.nextLine();
                 String[] recipeArray = line.split("\\|");
-                String prepTimeS = recipeArray[3];
-                String cookTimeS = recipeArray[4];
-                int prepTime = Integer.parseInt(prepTimeS);
-                int cookTime = Integer.parseInt(cookTimeS);
+                int prepTime = Integer.parseInt(recipeArray[3]);
+                int cookTime = Integer.parseInt(recipeArray[4]);
                 String[] recipeIngredients = recipeArray[5].split("@");
                 String[] recipeSteps = recipeArray[6].split("@");
                 Recipe newRecipe = new Recipe(recipeArray[0], recipeArray[1], prepTime, cookTime, recipeIngredients, recipeSteps, recipeArray[2]);
@@ -42,7 +37,7 @@ public class RecipeBook {
      * Gets the amount of recipes in the array.
      */
     public int getRecipeCount() {
-        return mCount;
+        return mRecipeBook.length;
     }
 
     /**
@@ -63,7 +58,8 @@ public class RecipeBook {
      * @return The index of the first recipe that contain the ingredient starting from the user's index, or -1 if no Recipe uses the ingredient.
      */
     public int findIngredient(String ingredient, int startingIndex) {
-        for(int i = startingIndex; i < mCount; i++) {
+        System.out.println("test");
+        for(int i = startingIndex; i < mRecipeBook.length; i++) {
             if(mRecipeBook[i].usesIngredient(ingredient)) {
                 return i;
             }
@@ -78,89 +74,5 @@ public class RecipeBook {
      */
     public int findIngredient(String ingredient) {
         return findIngredient(ingredient, 0);
-    }
-
-    private void resize() {
-        Recipe[] temp = new Recipe[2*mRecipeBook.length];
-        for (int i = 0; i < mRecipeBook.length; i++) {
-            temp[i] = mRecipeBook[i];
-        }
-        mRecipeBook = temp;
-    }
-
-    /**
-     * Adds a value to the end of the list, resizing if necessary.
-     * @param v The value that is added to the list.
-     */
-    public void addLast(Recipe v) {
-        if (mCount == mRecipeBook.length) {
-            resize();
-        }
-        mRecipeBook[mCount++] = v;
-    }
-
-    /**
-     * Adds a value to a specified index of the list, moving other items over and/or resizing if necessary.
-     * @param index The index that the value is placed in the list.
-     * @param v The value that is added to the list.
-     */
-    public void insert(int index, Recipe v) {
-        if (mCount == mRecipeBook.length) {
-            resize();
-        }
-        shiftRight(index);
-        mRecipeBook[index] = v;
-    }
-
-    /**
-     * Removes the value at a specified index in the list.
-     * @param index The index at which the value is removed.
-     */
-    public void removeAt(int index){
-        if (index >= mRecipeBook.length) {
-            throw new IndexOutOfBoundsException("There is no recipe at that index");
-        }
-        shiftLeft(index);
-    }
-
-    /**
-     * Searches through the list of recipe titles for the specified title, returning the index if it is found.
-     * @param title The recipe title that is being searched for in the list of recipes.
-     * @return The index of the found recipe, or -1 if no recipe is found in the list with the specified title.
-     */
-    public int findRecipeTitle(String title) {
-        for (int i = 0; i < mCount; i++) {
-            if(title.equals(mRecipeBook[i].getTitle())) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private void shiftRight(int index) {
-        if (mCount == mRecipeBook.length) {
-            resize();
-        }
-        Recipe[] temp = new Recipe[mRecipeBook.length];
-        for(int i = 0; i <= index; i++) {
-            temp[i] = mRecipeBook[i];
-        }
-        for(int i = index; i < mCount; i++) {
-            temp[i + 1] = mRecipeBook[i];
-        }
-        mCount++;
-        mRecipeBook = temp;
-    }
-
-    private void shiftLeft(int index) {
-        Recipe[] temp = new Recipe[mRecipeBook.length];
-        for(int i = 0; i < index; i++) {
-            temp[i] = mRecipeBook[i];
-        }
-        for(int i = index; i < mCount - 1; i++) {
-            temp[i] = mRecipeBook[i + 1];
-        }
-        mCount--;
-        mRecipeBook = temp;
     }
 }
